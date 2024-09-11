9ef762682234c4a053ff7bc6f864fdc24481b330
Nguyen0362 <nanhnguyen6789@gamil.com>
2024-09-11 21:10:49 +0700

    thay doi

---
+++ routes/client/index.route.js
@@ -0,0 +1,10 @@
+const productRoute = require('./product.route');
+
+module.exports = (app) => {
+    app.get("/", (req, res) => {
+        res.render("client/pages/home/index");
+    });
+    
+    app.use("/products", productRoute);
+}
+
