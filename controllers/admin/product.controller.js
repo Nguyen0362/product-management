21490a6a6e3dccf34e168685646cb9c3409101e1
Nguyen0362 <nanhnguyen6789@gamil.com>
2024-09-16 11:34:20 +0700

    Tính năng phân trang

---
+++ controllers/admin/product.controller.js
@@ -0,0 +1,50 @@
+const Product = require('../../model/product.model');
+
+module.exports.index = async (req, res) => {
+    const find = {
+        deleted: false
+    };
+
+    // Lọc theo trạng thái
+    if(req.query.status){
+        find.status = req.query.status;
+    }
+    // Hết lọc theo trạng thái
+
+    // Tìm Kiếm
+    if(req.query.keyword){
+        const regex = new RegExp(req.query.keyword, "i");
+        find.title = regex;
+    }
+    // Hết tìm Kiếm
+
+    // Phân trang
+    let limitItem = 4;
+    let page = 4;
+
+    if(req.query.page){
+        page = parseInt(req.query.page);
+    }
+
+    if(req.query.limit){
+        limitItem = parseInt(req.query.limit);
+    }
+
+    const skip = (page - 1) * limitItem;
+
+    const totalProduct = await Product.countDocuments(find);
+
+    const totalPage = Math.ceil(totalProduct / limitItem);
+
+    console.log(totalPage);
+    // Hết phân trang
+
+    const products = await Product.find(find).limit(limitItem).skip(skip);
+
+    res.render("admin/pages/products/index", {
+        pageTitle: "Trang danh sách sản phẩm",
+        products: products,
+        totalPage: totalPage,
+        currentPage: page
+    });
+}
---
+++ public/admin/js/script.js
@@ -0,0 +1,88 @@
+// bộ lọc
+const boxFilter = document.querySelector("[box-filter]");
+
+console.log(boxFilter);
+
+if(boxFilter){
+    let url = new URL(location.href);
+
+
+    boxFilter.addEventListener("change", () => {
+        const value = boxFilter.value;
+
+        if(value){
+            url.searchParams.set("status", value);
+        } else {
+            url.searchParams.delete("status");
+        }
+
+        location.href = url.href;
+    });
+
+    // Hiển thị lựa chọn mặc định
+    const statusCurrent = url.searchParams.get("status");
+    if(statusCurrent){
+        boxFilter.value = statusCurrent;
+    }
+    // Hết hiển thị lựa chọn mặc định
+}
+// Hết bộ lọc
+
+//Tìm kiếm
+const formSearch = document.querySelector("[form-search]");
+
+if(formSearch){
+    let url = new URL(location.href); //Nhân bản url
+
+    formSearch.addEventListener("submit", (event) => {
+        event.preventDefault(); // Ngăn chặn hành vi mặc định: submit form
+        const value = formSearch.keyword.value;
+
+        if(value) {
+        url.searchParams.set("keyword", value);
+        } else {
+        url.searchParams.delete("keyword");
+        }
+
+        location.href = url.href;
+    });
+
+    // Hiển thị từ khóa mặc định
+    const valueCurrent = url.searchParams.get("keyword");
+    if(valueCurrent){
+        formSearch.keyword.value = valueCurrent;
+    }
+    // Hết hiển thị từ khóa mặc định
+}
+// Hết tìm kiếm
+
+//Phân trang
+const listButtonPagination = document.querySelectorAll("[button-pagination]");
+
+if(listButtonPagination.length > 0){
+    let url = new URL(location.href);
+
+    listButtonPagination.forEach(button => {
+        button.addEventListener("click", () => {
+            const page = button.getAttribute("button-pagination");
+
+            if(page){
+                url.searchParams.set("page", page);
+            } else {
+                url.searchParams.delete("page");
+            }
+
+            location.href = url.href;
+        })
+    })
+
+    // Hiển thị trang mặc định
+    const pageCurrent = url.searchParams.get("page") || 1;
+    const buttonCurrent = document.querySelector(`[button-pagination="${pageCurrent}"]`);
+
+    if(buttonCurrent){
+        buttonCurrent.parentNode.classList.add("active");
+    }
+// Hết hiển thị trang mặc định
+}
+// Hết phân trang
